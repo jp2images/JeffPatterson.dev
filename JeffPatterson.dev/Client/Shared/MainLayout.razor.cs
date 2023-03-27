c6213bef1da4fc04bdeb57453c8a9cfc43113c23
Jeff <jeff_patterson@outlook.com>
2023-03-26 20:25:29 -0500

    working on the layout and a tiny bit of content.

---
+++ JeffPatterson.dev/Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JeffPatterson.dev.Client.Shared;
+public partial class MainLayout
+{
+  [Inject] private NotificationService NotificationService { get; set; }
+
+  private void ButtonClick(string clickedText)
+  {
+    NotificationService.Notify(new NotificationMessage
+    {
+      Severity = NotificationSeverity.Info,
+      Summary = "This Button Clicked",
+      Detail = clickedText
+    });
+  }
+
+  private void AnotherButtonClick()
+  {
+    NotificationService.Notify(new NotificationMessage
+    {
+      Severity = NotificationSeverity.Info,
+      Summary = "Another Button Clicked",
+      Detail = "Some string"
+    });
+  }
+}
--- JeffPatterson.dev/Client/Shared/NavMenu.razor.cs
+++ JeffPatterson.dev/Client/Shared/NavMenu.razor.cs
@@ -1,14 +1,22 @@
-﻿namespace JeffPatterson.dev.Client.Shared
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace JeffPatterson.dev.Client.Shared;
+public partial class NavMenu
 {
-  public partial class NavMenu
+  [Inject] protected TooltipService toolTipService { get; set; }
+
+
+  private bool collapseNavMenu = true;
+
+  private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+
+  private void ToggleNavMenu()
   {
-    private bool collapseNavMenu = true;
-
-    private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-      collapseNavMenu = !collapseNavMenu;
-    }
+    collapseNavMenu = !collapseNavMenu;
   }
+
+  void ShowTooltip(ElementReference elementReference, TooltipOptions options = null) => toolTipService.Open(elementReference, "Some content", options);
+
 }
